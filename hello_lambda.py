088fc77259a57eb5499c777d6bb1a514922961df
Oliver Choy <oliver.choy@slalom.com>
2020-09-29 01:33:04 -0700

    Dragon B

--- hello_lambda.py
+++ hello_lambda.py
@@ -3,7 +3,7 @@ import requests
 def lambda_handler(json_input, context):
     print(json_input)
     print(context)
-    print("Hello Dragon A")
+    print("Hello Dragon B")
 
 
 if __name__ == '__main__':
